02cdd52137fde82f7385b034d95ed3d40bbf5021
0dminnimda <0dminnimda@gmail.com>
2024-06-13 22:08:26 +0300

    Don't use list mode, add highlight for trailing characters

--- my_init.lua
+++ my_init.lua
@@ -1,11 +1,16 @@
 -- listchars=eol:↵,trail:~,tab:>-,nbsp:␣,tab:▷ ,trail:·,extends:◣,precedes:◢,nbsp:○
+--[[
 vim.opt.listchars:append({
   -- eol = "↵",
   nbsp = "␣",
   tab = "▷ ",
-  trail = "·",
+  trail = "%",  --✚■
   extends = "◣",
   precedes = "◢",
 })
-vim.opt.list = true 
+vim.opt.list = true
+]]--
+
+vim.api.nvim_set_hl(0, "EoLSpace", { bg="#FF0000" })
+vim.fn.matchadd("EoLSpace", "\\s\\+$")
 
