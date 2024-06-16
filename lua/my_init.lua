09c935547ba1a01a257ecb9ae4c841e66d52f76b
0dminnimda <0dminnimda@gmail.com>
2024-06-16 10:12:40 +0300

    Add commented url highlighting

--- lua/my_init.lua
+++ lua/my_init.lua
@@ -14,3 +14,9 @@ vim.opt.list = true
 vim.api.nvim_set_hl(0, "TrailingWhitespace", { bg="#FF0000" })
 vim.fn.matchadd("TrailingWhitespace", "\\s\\+$")
 
+-- TODO: add url highlighting
+-- For more strict, use: https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)
+-- (http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b[-a-zA-Z0-9@:%_\+.~#?&//=]*
+-- vim.api.nvim_set_hl(0, "UrlString", { underline=true })
+-- vim.fn.matchadd("UrlString", [[(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b[-a-zA-Z0-9@:%_\+.~#?&//=]*]])
+
