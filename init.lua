9dccfdedf69d0055c869c7151d85bdeafe96d62f
0dminnimda <0dminnimda@gmail.com>
2026-01-05 17:25:33 +0400

    Move lspconfig into its config

--- init.lua
+++ init.lua
@@ -23,29 +23,8 @@ local load_plugins = function()
     }, lazy_config)
 end
 
-local setup_diagnostics = function()
-    vim.diagnostic.config {
-      virtual_text = false, -- Turn off inline diagnostics
-    }
-
-    -- Use this if you want it to automatically show all diagnostics on the
-    -- current line in a floating window. Personally, I find this a bit
-    -- distracting and prefer to manually trigger it (see below). The
-    -- CursorHold event happens when after `updatetime` milliseconds. The
-    -- default is 4000 which is much too long
-    vim.cmd "autocmd CursorHold * lua vim.diagnostic.open_float({ focus=false })"
-    vim.o.updatetime = 300
-
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-      virtual_text = false,
-    })
-
-    -- diagnostics continued in mappings.lua --
-end
-
 bootstrap()
 require("options")
 require("themes")
 require("mappings")
-setup_diagnostics()
 load_plugins()
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -88,11 +88,14 @@ M.diagnostic_config = function()
   local x = vim.diagnostic.severity
 
   vim.diagnostic.config {
-    virtual_text = { prefix = "" },
+    virtual_text = false, -- { prefix = "" },
     signs = { text = { [x.ERROR] = "󰅙", [x.WARN] = "", [x.INFO] = "󰋼", [x.HINT] = "󰌵" } },
     underline = true,
     float = { border = "single" },
   }
+
+  -- Auto-show diagnostics in float on hover
+  vim.cmd "autocmd CursorHold * lua vim.diagnostic.open_float({ focus=false })"
 
   -- Default border style
   local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
